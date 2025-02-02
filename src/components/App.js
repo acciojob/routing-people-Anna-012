ff1ba47ece95cdd711cb244cb5ebc8e5ba535db8
Anna shabbagh <afreenshabbagh@gmail.com>
2025-02-02 19:23:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,28 @@
-import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import users from "./users";
-//
-import "./../styles/App.css";
-import UserList from "./UserList";
-import UserDetails from "./UserDetails";
+import React from 'react';
+import { BrowserRouter , Routes, Route, useParams} from 'react-router-dom';
+import Home from './Home';
+import UserDetails from './UserDetail';
+import users from './users';
+
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<UserList />} />
-        {
-          users.map((user, index) => (
-            <Route key={index} path={`/users/${user.id}`} element={<UserDetails userId={user.id} />} />
-          ))
-        }
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route
+            path="/users/:id"
+            element={<UserDetailsWrapper />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
+};
+const UserDetailsWrapper = (props) => {
+  const { id } = useParams();
+  const user = users.find(user => user.id === parseInt(id));
+  return user ? <UserDetails user={user} /> : <div>User not found</div>;
 };
 
 export default App;
